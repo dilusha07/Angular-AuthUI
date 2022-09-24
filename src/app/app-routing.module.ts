675ced42ba9274748c62aaa866466886d2f8b901
dilusha07 <dilushasandaruwani07@gmail.com>
2022-09-24 23:04:33 +0530

    Implement signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componets/login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
